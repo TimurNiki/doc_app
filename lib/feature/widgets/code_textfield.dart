a065bff4b8acdf87167bb899c9385848f74b7779
TimurNiki <timurniki95@gmail.com>
2023-11-27 13:11:47 +0400

    numberconfirmationdone

---
+++ lib/feature/widgets/code_textfield.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../core/constants/index.dart';
+
+class CodeTextField extends StatelessWidget {
+  const CodeTextField({
+    super.key,
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      style: AppFonts.w600s18.copyWith(
+        color: AppColors.black,
+      ),
+      textAlign: TextAlign.center,
+      obscureText: true,
+      keyboardType: TextInputType.phone,
+      obscuringCharacter: '*',
+      maxLength: 4,
+      decoration: InputDecoration(
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.textFieldColor,
+          ),
+        ),
+        errorStyle: AppFonts.w600s18.copyWith(
+          color: Colors.red,
+        ),
+        prefixText: AppStrings.code,
+        prefixStyle: AppFonts.w600s18.copyWith(color: AppColors.black),
+        suffixIcon: CircleAvatar(
+          backgroundColor: AppColors.iconBackGroundColor,
+          radius: 17,
+          child: IconButton(
+            onPressed: () {
+              controller.clear();
+            },
+            icon: const Icon(Icons.clear),
+          ),
+        ),
+      ),
+    );
+  }
+}
